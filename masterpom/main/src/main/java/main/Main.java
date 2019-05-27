32b1492df34f6899d3a0d9a7f2198700690fbb35
YannMelt237 <yannmelt4@gmail.com>
2019-05-27 14:01:52 +0200

    modification pour pouvoir committer independamment

--- masterpom/main/src/main/java/main/Main.java
+++ masterpom/main/src/main/java/main/Main.java
@@ -1,13 +1,11 @@
-/**
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
 package main;
 
-import contract.ControllerOrder;
+
 import controller.Controller;
-import model.Model;
 import view.View;
+import model.Model;
+
+
 
 /**
  * The Class Main.
@@ -22,13 +20,11 @@ public abstract class Main {
      * @param args
      *            the arguments
      */
-    public static void main(final String[] args) {
-        final Model model = new Model();
-        final View view = new View(model);
-        final Controller controller = new Controller(view, model);
-        view.setController(controller);
-
-        controller.control();
-        controller.orderPerform(ControllerOrder.English);
+	 public static void main(final String[] args) {
+      
+		 final Controller controller = new Controller(new View(), new Model());
+              
+		controller.start();
+		System.out.println("Ca marche");
     }
 }
